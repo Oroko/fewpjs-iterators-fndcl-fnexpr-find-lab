22144720065c4e8f304142c71f63c72f9dd1ee35
Oroko <aoroko09@gmail.com>
2021-04-18 11:16:25 -0400

    commit tree b3ec2a82a3c0577a4a3ed87b850e008709f9d063

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 
+// const record = [
+//   { year: "2018", result: "L" },
+//   { year: "2017", result: "W" },
+//   { year: "2016", result: "N/A" },
+//   //...
+// ];
+
+const superbowlWin = (array) => {
+
+
+  let w = array.find(w => w.result === "W") 
+  return w ? w.year : undefined
+  
+  
+}
+
+
+
